ea20547b9da20accd7502a9ca80112843ff73b28
Chris Paterson <chrispaterson22@gmail.com>
2021-02-23 12:10:06 +0000

    initial commit weather app expo RN

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View, Animated } from 'react-native'
+import Weather from './components/Weather'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+	state = {
+		isLoading: false,
+	}
+
+	render() {
+		const { isLoading } = this.state
+		return (
+			<View style={styles.container}>
+				{isLoading ? <Text>Fetching The Weather</Text> : <Weather />}
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})
